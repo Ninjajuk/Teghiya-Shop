3fb7551b79b8984b6c1ec8590d6e6bb7cd5fad90
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2024-09-24 00:37:54 +0530

    cart

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,6 +1,6 @@
 'use client'
 import { Fragment, useState } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
+
 import { XMarkIcon } from '@heroicons/react/24/outline'
 import { useSelector, useDispatch } from "react-redux";
 import {calculateSubtotal} from '../utility/cartUtils'
@@ -8,34 +8,11 @@ import { increaseQuantity,decreaseQuantity,removeItem,login} from "../redux/cart
 import Link from 'next/link';
 import EmptyCart from './EmptyCart';
 
-// const products = [
-//   {
-//     id: 1,
-//     name: 'Throwback Hip Bag',
-//     href: '#',
-//     color: 'Salmon',
-//     price: '$90.00',
-//     quantity: 1,
-//     imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg',
-//     imageAlt: 'Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.',
-//   },
-//   {
-//     id: 2,
-//     name: 'Medium Stuff Satchel',
-//     href: '#',
-//     color: 'Blue',
-//     price: '$32.00',
-//     quantity: 1,
-//     imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg',
-//     imageAlt:
-//       'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
-//   },
-//   // More products...
-// ]
+
 
 export default function ShoppingCart() {
 
-  const [open, setOpen] = useState(true)
+  const [open, setOpen] = useState(false)
   const cartItems = useSelector((state) => state.cart);
   const isLoggedIn = useSelector((state) => state.auth);
   const subtotal = calculateSubtotal(cartItems);
@@ -56,30 +33,24 @@ export default function ShoppingCart() {
     }
   };
 
+  
+
   return (
     <>
       {cartItems.length === 0 ? (
         <EmptyCart />
       ) : (
-        <Transition.Root show={open} as={Fragment}>
-          {}
-          <Dialog as="div" className="relative z-10" onClose={setOpen}>
-            <Transition.Child
-              as={Fragment}
-              enter="ease-in-out duration-500"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="ease-in-out duration-500"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-            </Transition.Child>
+          <div  className="relative z-10"              
+          onClick={(e) => {
+            e.stopPropagation(); // Prevent bubbling
+            setOpen(false);
+          }}>
+
 
             <div className="fixed inset-0 overflow-hidden">
               <div className="absolute inset-0 overflow-hidden">
                 <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
-                  <Transition.Child
+                  <div
                     as={Fragment}
                     enter="transform transition ease-in-out duration-500 sm:duration-700"
                     enterFrom="translate-x-full"
@@ -88,13 +59,13 @@ export default function ShoppingCart() {
                     leaveFrom="translate-x-0"
                     leaveTo="translate-x-full"
                   >
-                    <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
+                    <div className="pointer-events-auto w-screen max-w-md">
                       <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
                         <div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
                           <div className="flex items-start justify-between">
-                            <Dialog.Title className="text-lg font-medium text-gray-900">
+                            <h1 className="text-lg font-medium text-gray-900">
                               Shopping cart
-                            </Dialog.Title>
+                            </h1>
                             <div className="ml-3 flex h-7 items-center">
                               <button
                                 type="button"
@@ -204,13 +175,13 @@ export default function ShoppingCart() {
                           </div>
                         </div>
                       </div>
-                    </Dialog.Panel>
-                  </Transition.Child>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </Dialog>
-        </Transition.Root>
+          </div>
+
       )}
     </>
   );
